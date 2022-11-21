80b195e49254d000ff4d16d58533892eab2fe223
Charles POULAIN <charles.poulain@roav7.com>
2022-11-21 18:05:22 +0100

    Added number_guesser

---
+++ number_guesser.py
@@ -0,0 +1,40 @@
+import random
+import os
+
+
+def generateRrandom():
+    return random.randint(1, 10)
+
+
+def restartGame():
+    userInput = input("Do you want to restart ? [Y/n] ")
+    if userInput.lower() == "y":
+        startGame()
+    else:
+        print("Goodbye !")
+
+
+def startGame():
+    os.system('cls')
+    numberToFind = generateRrandom()
+
+    while True:
+        userGuess = input("Make a guess: ")
+
+        if userGuess.isdigit():
+            userGuess = int(userGuess)
+        else:
+            print("Please type a number")
+            continue
+
+        if userGuess == numberToFind:
+            print("You got it !")
+            restartGame()
+            break
+        elif userGuess > numberToFind:
+            print("You were above the number !")
+        else:
+            print("You were below the number !")
+
+
+startGame()
